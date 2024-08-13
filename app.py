fdcba385640c57a0523e16058a0fb91edd498830
Fasil | Python/Odoo Developer <fasilwdr@hotmail.com>
2024-08-14 00:48:56 +0300

    Test 4

--- app.py
+++ app.py
@@ -11,15 +11,21 @@ session_id = cookies.get('session')
 
 full_url = f"{url if url[-1] != '/' else url[:-1]}/product/stock"
 
-command = f"python3 commix.py -u {full_url} --cookie=session=\"{session_id}\" -d \"productId=1&storeId=1\" --batch"
+command = [
+    "python3", "commix.py",
+    "-u", full_url,
+    "--cookie", f'session="{session_id}"',
+    "-d", "productId=1&storeId=1",
+    "--batch"
+]
 
-print("Executing command: ", command)
+print("Executing command: ", " ".join(command))
 
 with open("output.txt", "w", encoding='utf-8') as output_file:
     output_file.write(f"Affected URL : {full_url}\n")
 
 try:
-    result = subprocess.run(command, shell=True, capture_output=True, text=True, timeout=60)  # 60 seconds timeout
+    result = subprocess.run(command, capture_output=True, text=True, timeout=60)
     for line in result.stdout.splitlines():
         print(line)
         if "injectable" in line.lower() or "injection" in line.lower():
